b5194f09052ba9a5a069e37fd677f8ab00b9bf9f
gangjuice <kprodric@gmail.com>
2026-06-15 13:13:07 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-});
 const { app, BrowserWindow, ipcMain } = require('electron');
 const fs = require('fs');
 const path = require('path');
 
-// 📌 사내 PC의 '내 문서' 폴더 안에 '업무관리_데이터.json' 이라는 이름으로 영구 저장됩니다.
 const dataFilePath = path.join(app.getPath('documents'), '업무관리_데이터.json');
 
 function createWindow() {
@@ -11,19 +9,17 @@ function createWindow() {
         width: 1300,
         height: 900,
         webPreferences: {
-            nodeIntegration: true,     // HTML에서 PC 폴더 접근 권한 부여
+            nodeIntegration: true,
             contextIsolation: false
         }
     });
     
-    // 상단 기본 메뉴바 숨기기 (더 깔끔한 프로그램 UI)
     mainWindow.setMenuBarVisibility(false);
     mainWindow.loadFile('index.html');
 }
 
 app.whenReady().then(createWindow);
 
-// 📌 HTML에서 'load-data'를 요청하면 파일에서 데이터를 읽어옵니다.
 ipcMain.handle('load-data', () => {
     if (fs.existsSync(dataFilePath)) {
         return fs.readFileSync(dataFilePath, 'utf8');
@@ -31,7 +27,6 @@ ipcMain.handle('load-data', () => {
     return null; 
 });
 
-// 📌 HTML에서 'save-data'를 요청하면 파일에 즉시 덮어씁니다. (알림창 없음)
 ipcMain.on('save-data', (event, data) => {
     fs.writeFileSync(dataFilePath, data);
 });
